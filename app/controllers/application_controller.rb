dcdf1dbae1c11419ed4231ca61071055d1c509ce
SwapnilPatil <swapnil.patil04@gmail.com>
2017-03-10 19:34:37 +0530

    work on UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource) 
     "/groups"
   end 
@@ -10,4 +12,13 @@ class ApplicationController < ActionController::Base
   def after_sign_up_path_for(resource) 
     "/groups"
   end 
+
+  protected
+
+  def configure_permitted_parameters
+    # Fields for sign up
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :mobile) }
+    # Fields for editing an existing account
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :current_password) }
+  end
 end
